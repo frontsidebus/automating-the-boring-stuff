31c136e23bae0648297959338c56989acd4909fe
frontsidebus <paulebryant@gmail.com>
2020-04-04 22:13:14 -0500

    adding more examples from chapter 2 flow control

---
+++ swordfish.py
@@ -0,0 +1,9 @@
+while True:
+    print('Who are you?')
+    name = input()
+    if name != 'Joe':
+        continue
+    print('Hello, Joe. What is the password? (It is a Fish.)')
+    if password == 'swordfish':
+        break
+print('Access granted.')
