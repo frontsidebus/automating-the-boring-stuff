ec464751f290f7a1089f117efee337f04c48a4f8
frontsidebus <paulebryant@gmail.com>
2020-04-04 22:25:56 -0500

    for and while loop examples combined, debug statements added

--- fiveTimes.py
+++ fiveTimes.py
@@ -1,3 +1,12 @@
+print('Here is a for loop: ')
 print('My name is')
 for i in range(5):
     print('Jimmy Five Times (' + str(i) + ')')
+
+print('Here is the same thing with a while loop: ')
+print('My name is')
+i = 0
+while i < 5:
+    print('Jimmy Five Times (' + str(i) + ')')
+    i = (i + 1)
+ 
