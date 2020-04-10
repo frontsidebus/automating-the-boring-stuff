97a5226499f3cb43ea8a95dda2c2591c8b0c25fa
frontsidebus <paulebryant@gmail.com>
2020-04-10 12:46:29 -0500

    added in exercises

---
+++ exercise2-13.py
@@ -0,0 +1,9 @@
+print('This is a for loop: ')
+for number in range(1, 11):
+     print(int(number))
+
+print('This is a while loop: ')
+number = 1
+while number <= 10:
+    print(int(number))
+    number = (number + 1)
---
+++ exercise2-9.py
@@ -0,0 +1,10 @@
+print('What is spam?')
+
+spam = int(input())
+
+if spam == 1:
+    print('Hello!')
+elif spam == 2:
+    print('Howdy!')
+elif (spam != 1) or (spam != 2):
+    print('Greetings!') 
